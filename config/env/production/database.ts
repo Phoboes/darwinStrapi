1228b6d43f8e5548c6c81988cbc8eeb4ca1f595b
Kane <Phoboes@live.com>
2025-02-15 18:27:46 +1000

    Railway plzz

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -1,14 +1,25 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+module.exports = ({ env }) => {
+  const dbUrl = env('DATABASE_URL')
+  console.log('Database URL in config:', dbUrl)
+
+  return {
     connection: {
-      connectionString: env('DATABASE_PRIVATE_URL'),
-      ssl: {
-        rejectUnauthorized: false,
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', 'shortline.proxy.rlwy.net'),
+        port: env.int('DATABASE_PORT', 51244),
+        database: env('DATABASE_NAME', 'railway'),
+        user: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', 'yHSaGLwIafhPlVfqRlmRzfEdKzEgqwgt'),
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
+      pool: {
+        min: 0,
+        max: 5,
+      },
+      acquireConnectionTimeout: 5000,
     },
-    pool: {
-      min: 0,
-    },
-  },
-})
+  }
+}
