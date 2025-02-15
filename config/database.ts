efa3e348174a01d342375539aca8347cfe0bb794
Kane <Phoboes@live.com>
2025-02-15 18:10:55 +1000

    Local config.

--- config/database.ts
+++ config/database.ts
@@ -1,9 +1,15 @@
-module.exports = ({ env }) => ({
+const config = {
   connection: {
     client: 'postgres',
     connection: {
-      connectionString: env('DATABASE_PRIVATE_URL'),
+      host: process.env.DATABASE_HOST || '127.0.0.1',
+      port: Number(process.env.DATABASE_PORT) || 5432,
+      database: process.env.DATABASE_NAME || 'darwinStrapi',
+      user: process.env.DATABASE_USERNAME || 'admin',
+      password: process.env.DATABASE_PASSWORD || 'Password123',
+      ssl: false,
     },
-    pool: { min: 0 },
   },
-})
+}
+
+export default ({ env }) => config
